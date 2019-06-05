32b36664aa9509d2428f61b6dd8e1f1ae8b9badd
CI-Walter <walter.huang@cloud-interactive.com>
2019-06-05 18:31:14 +0800

    nuxt init and test webview work

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/vue'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'handle-callback-err': 'off',
+    'no-template-curly-in-string': 'off',
+    'camelcase': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'error',
+    'nuxt/no-globals-in-created': 'off'
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<style lang="stylus">
+span {
+  margin: 20px;
+  border: 1px solid black;
+}
+</style>
+<template lang="pug">
+.template
+  span(@click="callIos") Call IOS
+  span(@click="callAndroid") Call Androide
+
+  div 成功： {{message}}
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      message: '未連通',
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {
+    window.dialog = this.dialog
+    // const dialog = this.dialog
+  },
+  mounted() {},
+  methods: {
+    dialog(sString) {
+      // alert(`call javascript method is ${sString}`)
+      this.message = `連通${sString}`
+    },
+    callIos() {
+      window.webkit.messageHandlers.ToApp.postMessage('SendmessagetoSwift')
+    },
+    callAndroid() {
+      // alert(window.native)
+      window.native.hello2('abcdefg', 123)
+      // document.location = 'js://webview?arg1=234&arg2=987'
+      // document.location = 'js://xxx?id=asd&name=qwe'
+    },
+  },
+}
+</script>
